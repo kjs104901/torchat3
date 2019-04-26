4c8b45cef419bd5f4d36b12524701e03a00532ae
kjs104901 <kjs104901@gmail.com>
2019-04-26 17:19:23 +0900

    bootstrap init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # torchat3
+Electron-based torchat
+
+## Requirements
+
+git: https://git-scm.com/downloads
+
+node.js: https://nodejs.org
+
+## Execution
+
+```
+git clone https://github.com/kjs104901/torchat3.git torchat3
+cd torchat3
+npm install
+
+```
+
+## Usage
+
+```
+npm start
+```
+or
+```
+electron .
+```
--- pages/main.js
+++ pages/main.js
@@ -2,12 +2,23 @@ const { remote, ipcRenderer } = require('electron');
 const { BrowserWindow } = remote;
 
 window.$ = window.jQuery = require('jquery');
+require('popper.js');
+require('bootstrap');
+
 
 setInterval(() => {
     ipcRenderer.send('bootInfoReq');
 }, 100);
 
+let progress = 0;
+let logs = [];
 ipcRenderer.on('bootInfoRes', (event, message) => {
-    $("#tor-status").text(message.progress);
-    $("#tor-logs").html(message.logs.join('<br><br>'))
+    if (progress != message.progress) {
+        progress = message.progress;
+        $("#tor-status").text(progress);
+    }
+    if (logs.length < message.logs.length) {
+        logs = message.logs;
+        $("#tor-logs").html(logs.join('<br><br>'));
+    }
 })
